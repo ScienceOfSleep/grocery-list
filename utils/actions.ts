a9a4a0fcfe26e911e13608ab983e37fc8cbab815
Josh <jjsteele@ualberta.ca>
2024-01-13 16:28:17 -0700

    Relaxed TS

--- utils/actions.ts
+++ utils/actions.ts
@@ -5,18 +5,24 @@ import db from './db'
 // CRUD Actions
 
 // Create
-export const newListItem = async (formData) => {
-    const listItem = await db.listItem.create({
-        data: {
-            content: formData.get('content'),
-        },
-    })
+export const newListItem = async (formData: FormData) => {
+  const content = formData.get('content');
 
-    revalidatePath('/')
+  if (typeof content !== 'string') {
+      throw new Error('Content is not a string');
+  }
+
+  const listItem = await db.listItem.create({
+      data: {
+          content: content,
+      },
+  });
+
+  revalidatePath('/');
 }
 
 // Update
-export const checkItem = async (id) => {
+export const checkItem = async (id: string) => {
     await db.listItem.update({
         where: { id },
         data: {
@@ -28,7 +34,7 @@ export const checkItem = async (id) => {
 }
 
 // Delete 1
-export const deleteItem = async (id) => {
+export const deleteItem = async (id: string) => {
     await db.listItem.delete({
         where: { id }
     })
